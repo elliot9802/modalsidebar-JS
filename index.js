dc43021382be9396b752021bb21a0eef81f9b7b5
elliot9802 <elliot@segerlind.se>
2022-01-11 23:13:25 +0100

    fixed modal and sidebar

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 // Modal
 let modal = document.getElementById("modalContainer");
 let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("closeBtn")[0];
+let close = document.getElementsByClassName("closeBtn")[0];
 
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-span.onclick = function () {
+close.onclick = function () {
   modal.style.display = "none";
 };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
+window.onclick = function (e) {
+  if (e.target === modal) {
     modal.style.display = "none";
   }
 };
 
 // Sidebar
+// let sidebar = document.getElementById("sidebarContainer");
+// let sidebarBtn = document.getElementsByClassName("openbtn");
+
+// window.onclick = function (e) {
+//   if (e.target !== sidebar || sidebarBtn) {
+//     closeNav();
+//   }
+// };
+
 function openNav() {
   document.getElementById("sidebarContainer").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
